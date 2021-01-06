5af515f6ac9f1810646653ace4339f0b999e0c75
Omeed Naraghi-Pour <omeed.naraghipour@gmail.com>
2021-01-06 05:29:35 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -19,7 +19,10 @@ def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
   
-  
+  constants = [
+    { name:  "Don Gately", occupation: "Live-in Staff"}
+    
+    ]
 end
 
 def aoh_lookup(aoh, row, key)
