1c1ebf515af4ba9fade8992d615f411145b226f3
Omeed Naraghi-Pour <omeed.naraghipour@gmail.com>
2021-01-06 05:32:44 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -25,7 +25,6 @@ def literal_aoh
     {:name => "Pat Monteseian", :occupation => "Staff" }
     {:name => "Kate Gompert", :occupation => "None" }
     {:name => "Bruce Green", :occupation => "Fan of Mildred" }
-    
     ]
 end
 
