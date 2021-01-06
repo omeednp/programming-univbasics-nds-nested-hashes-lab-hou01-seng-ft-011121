94710a2d24b1d083783cf55c9d74827e148cb739
Omeed Naraghi-Pour <omeed.naraghipour@gmail.com>
2021-01-06 05:28:07 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -12,11 +12,14 @@ def assembled_aoh
   
   constants = [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
   return constants
+  
 end
 
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
+  
+  
 end
 
 def aoh_lookup(aoh, row, key)
